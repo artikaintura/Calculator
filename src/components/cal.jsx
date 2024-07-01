82a4f7760ab72eac4962fdfeea81ebc865589d94
arti kaintura <artikaintura274@gmail.com>
2024-07-01 15:20:48 +0530

    all are added

--- src/components/cal.jsx
+++ src/components/cal.jsx
@@ -1,58 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './cal.css';
+import Output from './output';
 
-
-const cal =(props)=>{
-    const handleClick =() =>{
+const Cal = (props) => {
+    let [input, setInput] = useState('0')
+    let [result, setResult] = useState('')
+    const handleClick = (event) => {
+        const value = event.target.value;
+        if (value === '=') {
+           if (input !== '') {
+                let res = ''
+                try {
+                    res = eval(input)
+                } catch (err) {
+                    setResult('Math Error')
+                }
+                if (res === undefined) {
+                    setResult('Math Error')
+                }
+                else {
+                    setResult(input + '=');
+                    setInput(res);
+                }
+            }
+        }
+        else if (value === 'C') {
+            setInput('0');
+            setResult('');
+        }
+        else if (value === 'DEL') {
+            let str = input;
+            str = str.substr(0, str.length - 1)
+            setInput(str);
+        }
+        else if (input === '0') {
+            setInput(value);
+        }
+        else {
+            setInput((input += value));
+        }
 
     }
-    return(
+    return (
         <div className="frame">
             <div className="calculator">
-                <br/>
+                <br />
+                <Output user={input} answer={result} />
+                <p> <span>My</span>Calculator</p>
                 <div className="keys">
                     <input type='button' value={'C'} className='button clear'
-                    onClick={handleClick} />
+                        onClick={handleClick} />
                     <input type='button' value={'DEL'} className='button clear'
-                    onClick={handleClick} />
+                        onClick={handleClick} />
                     <input type='button' value={'%'} className='button operator'
-                    onClick={handleClick} />
+                        onClick={handleClick} />
                     <input type='button' value={'/'} className='button operator'
-                    onClick={handleClick} />
+                        onClick={handleClick} />
 
                     <input type='button' value={'7'} className='button '
-                    onClick={handleClick}/>
+                        onClick={handleClick} />
                     <input type='button' value={'8'} className='button '
-                    onClick={handleClick}/>
+                        onClick={handleClick} />
                     <input type='button' value={'9'} className='button '
-                    onClick={handleClick} />
+                        onClick={handleClick} />
                     <input type='button' value={'*'} className='button operator'
-                    onClick={handleClick} />
-                    
+                        onClick={handleClick} />
+
                     <input type='button' value={'4'} className='button '
-                    onClick={handleClick} />
+                        onClick={handleClick} />
                     <input type='button' value={'5'} className='button '
-                    onClick={handleClick}/>
+                        onClick={handleClick} />
                     <input type='button' value={'6'} className='button '
-                    onClick={handleClick} />
+                        onClick={handleClick} />
                     <input type='button' value={'-'} className='button operator'
-                    onClick={handleClick}/>
-                    
+                        onClick={handleClick} />
+
                     <input type='button' value={'1'} className='button '
-                    onClick={handleClick} />
+                        onClick={handleClick} />
                     <input type='button' value={'2'} className='button '
-                    onClick={handleClick} />
+                        onClick={handleClick} />
                     <input type='button' value={'3'} className='button '
-                    onClick={handleClick} />
+                        onClick={handleClick} />
                     <input type='button' value={'+'} className='button operator'
-                    onClick={handleClick} />
-                    
+                        onClick={handleClick} />
+
                     <input type='button' value={'0'} className='button '
-                    onClick={handleClick} />
+                        onClick={handleClick} />
                     <input type='button' value={'.'} className='button '
-                    onClick={handleClick} />
-                    <input type='button' value={'='} className='button operator'
-                    onClick={handleClick} />
+                        onClick={handleClick} />
+                    <input type='button' value={'='} className='button equal-sign'
+                        onClick={handleClick} />
 
 
 
@@ -63,4 +100,4 @@ const cal =(props)=>{
         </div>
     )
 };
-export default cal;
+export default Cal;
